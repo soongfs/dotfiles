0b29fa163c6df894b8c696a0ed4d74f542c4da1f
soongfs <soongfs@proton.me>
2025-11-15 17:34:04 +0800

    fish, starship: add helix alias 'hx' and configure starship
    - Created 'hx' alias for helix editor in fish shell
    - Activated starship prompt in fish shell
    - Added starship configuration to the repository

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,5 @@ zoxide init fish | source
 if status is-interactive
     eval (zellij setup --generate-auto-start fish | string collect)
 end
+
+starship init fish | source
--- fish/.config/fish/functions/lt.fish
+++ fish/.config/fish/functions/lt.fish
@@ -1,6 +1,6 @@
 function lt
     if type -q eza
-        eza --icons -T $argv
+        eza --icons -aT $argv
     else
         echo "eza not found"
     end
