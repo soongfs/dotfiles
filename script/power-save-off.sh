f36d27d09018009cf803789515e3571d22de4c9f
soongfs <soongfs@proton.me>
2025-11-16 20:33:38 +0800

    script: add two scripts to control power profiles

---
+++ script/power-save-off.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+powerprofilesctl set balanced
+
+niri msg output eDP-1 mode 2560x1440@165.003
---
+++ script/power-save-on.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+powerprofilesctl set power-saver
+
+niri msg output eDP-1 mode 2560x1440@60.000
