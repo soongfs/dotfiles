c02572f1cfe4b48dd17c9e8a2234a9efc8044050
soongfs <soongfs@proton.me>
2025-11-19 11:59:11 +0800

    fish: disable zellij and starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,10 @@ end
 
 zoxide init fish | source
 
-if status is-interactive
-    eval (zellij setup --generate-auto-start fish | string collect)
-end
+# if status is-interactive
+#     eval (zellij setup --generate-auto-start fish | string collect)
+# end
 
-starship init fish | source
+# starship init fish | source
 
 set -gx PATH $HOME/.local/bin $PATH
